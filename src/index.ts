840c02fb96a670b14f4d94fce8b1347e3b7a4cf2
unknown <marchetti2@gmail.com>
2021-08-24 16:53:00 -0300

    added Error class handlling - AppError

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import './database'
 import './shared/container';
 
 import { usersRoutes } from "./routes/users.router";
+import { AppError } from "./shared/errors/AppError";
 
 const app = express();
 
 app.use(express.json());
 app.use("/users", usersRoutes);
+
+app.use(
+  (error: Error, _: Request, response: Response, _next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${error.message}`,
+    });
+  }
+);
 
 export { app };
